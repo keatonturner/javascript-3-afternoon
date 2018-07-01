004108d7ae325d77747159e3e46f2ad4376e1ab8
Keaton turner <keatonturner@Keatons-MacBook-Pro.local>
2018-07-01 16:21:50 -0600

    second commit

--- for-in-delete.js
+++ for-in-delete.js
@@ -10,14 +10,13 @@
   First we'll look at the difference between accessing property values in a for in loop and accessing the property name in a for in loop.
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
-
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+} 
 
 // for(var key in values) {
 //   console.log(values[key])
@@ -40,8 +39,13 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  let arr = [];
+  for(var key in obj){
+     arr.push(obj[key]);
+  }
+  return arr.join('')
 }
+showValues(values);
 
 
 
@@ -52,8 +56,21 @@ function showValues( obj ) {
   Write a for in loop that loops over the object and changes any value that is great than 10 to 0. 
   Return the updated object.
 */
-
-//Code Here
+var Object = {
+  one: 1,
+  two: 2,
+  three: 13,
+  four: 14,
+  five: 5
+}
+function greaterThan10(obj) {
+  for(var key in obj){
+    if(obj[key] > 10){
+      return obj[key] = 0;
+    }
+  }
+  return obj;
+}
 
 
 
